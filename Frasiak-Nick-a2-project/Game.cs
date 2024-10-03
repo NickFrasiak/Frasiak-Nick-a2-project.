45bb523738fe76db40edcdfaec2caea460789ff3
Nicholas Frasiak <000959762@mohawkcollege.ca>
2024-10-03 16:08:20 -0400

    building coords

--- Frasiak-Nick-a2-project/Game.cs
+++ Frasiak-Nick-a2-project/Game.cs
@@ -13,14 +13,18 @@ namespace Game10003
         // Place your variables here:
         //colors
         Color skyColor = new Color(0x4d, 0x4d, 0x80);
-        Color moonColor1 = new Color(0x44, 0x4a, 0x65);
-        
-        Color moonColor3 = new Color(0xf9, 0xc2, 0x2b);
-        Color moonColor4 = new Color(0Xff, 0Xd6, 0x13);
+        Color moonColourY1 = new Color(0Xff, 0Xf4, 0X4a);
+        Color moonColorY2 = new Color(0xfa, 0xa2, 0x11);
+        Color moonColorB1 = new Color(0xa0, 0xde, 0xda);
+        Color moonColorB2 = new Color(0x69, 0xa0, 0xc7);
+        Color moonColorB3 = new Color(0x30, 0x32, 0x4d);
+        Color moonColorW = new Color(0Xff, 0Xf9, 0xf0);
+        Color moonColorP = new Color(0x62, 0x1c, 0x80);
         Color buildingColor = new Color(0X84, 0X78, 0x75);
         Color buildingLightColor = new Color(0xff, 0xf4, 0x4a);
 
         //Buildings
+        float[][] buildingCoords = [[0, 220],[40 , 200], [80, 280],[120, 300], [160, 260],[240, 300], [320, 200], [360,200]];
 
         
 
